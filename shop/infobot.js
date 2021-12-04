e7d8ae1c904dbb5596addd48a4dd877318414401
SANGSANINNN26 <93709693+SANGSANINNN26@users.noreply.github.com>
2021-12-04 17:36:59 +0700

    Update infobot.js

--- shop/infobot.js
+++ shop/infobot.js
@@ -1,35 +1,35 @@
 const infobot = (latensii, totalchat, giid, wa_version, mcc, mnc, os_version, device_manufacturer, device_model, ownerNumberrr,runtime, kyun) => {
 return `ℒ⃝👾 *B O T  S T A T I S T I C*
 
-👨‍💻 *Owner : @${ownerNumberrr.split('@')[0]}*
-👨‍👨‍👧‍👦 *Group Chat : ${giid.length}*
-👥 *Personal Chat : ${totalchat.length - giid.length}*
-👨‍👩‍👧‍👧 *Total Chat : ${totalchat.length}*
-🍃 *Speed : ${latensii.toFixed(4)} Second (s)* 
-🌾 *Runtime : ${kyun(runtime)}*
+あ *Owner : @${ownerNumberrr.split('@')[0]}*
+あ *Group Chat : ${giid.length}*
+あ *Personal Chat : ${totalchat.length - giid.length}*
+あ *Total Chat : ${totalchat.length}*
+あ *Speed : ${latensii.toFixed(4)} Second (s)* 
+あ *Runtime : ${kyun(runtime)}*
 
 
-ℒ⃝📲 *P H O N E  S T A T I S T I C*
+ℒ⃝あ *P H O N E  S T A T I S T I C*
 
-🪀 *WhatsApp V : ${wa_version}*
-🛑 *RAM : ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}MB / ${Math.round(require('os').totalmem / 1024 / 1024)}MB*
-📈 *MCC : ${mcc}*
-📉 *MNC : ${mnc}*
-📊 *OS Version : ${os_version}*
-📫 *Merk Hp : ${device_manufacturer}*
-📮 *Versi Hp : ${device_model}*
+あ *WhatsApp V : ${wa_version}*
+あ *RAM : ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}MB / ${Math.round(require('os').totalmem / 1024 / 1024)}MB*
+あ *MCC : ${mcc}*
+あ *MNC : ${mnc}*
+あ *OS Version : ${os_version}*
+あ *Merk Hp : ${device_manufacturer}*
+あ *Versi Hp : ${device_model}*
 
 
-ℒ⃝👨‍💻 *C O N N E C T  W I T H  M E*
+ℒ⃝あ *C O N N E C T  W I T H  M E*
 
-⛎ Api : api-alphabot.herokuapp.com
-♈ YouTube : youtube.com/c/zeeoneofc
-♉ Github : zeeoneofc.github.io
-♊ Instagram : instagram.com/zeeoneofc
-♋ Tiktok : tiktok.com/@zeeoneofc
-♌ Telegram : t.me/zeeoneofc
-♎ Ch Telegram : t.me/zeeone_ofc
-♏ WhatsApp : bit.ly/zeeone_ofc
+⛎ mobile legend : SANG SANINNN
+♈ YouTube : youtube.com/c/AnangGamteng
+♉ Github : SANGSANINNN26.github.io
+♊ Instagram : instagram.com/ananglimit26
+♋ Tiktok : tiktok.com/@AnangGamteng
+♌ Telegram : t.me/AnangGamteng
+♎ Ch Telegram : t.me/Anang_Gamteng
+♏ WhatsApp : wa.me/6281267439077?text=Halo+Bang+Anang+Gamteng
 `}
 
 exports.infobot = infobot
