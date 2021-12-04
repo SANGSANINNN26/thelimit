9803e2ebf72cc6521941f64f1f8afca7a12310a9
SANGSANINNN26 <anangfauzan26@gmail.com>
2021-12-04 17:24:49 +0700

    first commit

---
+++ shop/allpayment.js
@@ -0,0 +1,35 @@
+const allpayment = (prefix) => {
+return `PAYMENT BY ZEEONE OFC
+
+*🏧 BANK*
+
+1. BRI 
+	ㅁ 8881 0812 6743 9077
+2. BCA 
+	ㅁ 3901 0812 6743 9077
+3. BNI 
+	ㅁ 881 0812 6743 9077
+4. PERMATA
+	ㅁ 852 0812 6743 9077
+	
+*💰 E-MONEY*
+
+1. GOPAY
+	ㅁ 081267439077
+2. OVO
+	ㅁ 081267439077
+3. DANA
+	ㅁ 081267439077
+	
+Semuanya atas nama Anang&Fauzan
+
+*📲 PULSA*
+
+Tidak tersedia
+	
+Sebelum melakukan pembayaran ada baiknya anda menghubungi owner terlebih dahulu!
+`
+	}
+
+exports.allpayment = allpayment
+ 
---
+++ shop/grupbot.js
@@ -0,0 +1,12 @@
+// jangan di ubah jika tidak punya group bot, takutnya eror
+const gcbotwa = () =>{
+	return`Join Aja Semua Fitur Bot Bisa Digunakan !
+
+1. *Nsfw Grup*
+_https://chat.whatsapp.com/CvwZp5Sopwr7KcU0P2kYmk
+
+Jika ada link yang ke reset, silahkan hubungi
+owner untuk meminta link yang baru
+`
+}
+exports.gcbotwa = gcbotwa
